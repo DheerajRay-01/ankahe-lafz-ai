0c31edbf3d488c49bec457cf405de36f63ecbe7b
DheerajRay-01 <dheerajray2003@gmail.com>
2025-03-05 18:30:14 +0530

    fix vit.config

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   server: {
     host: true,
-    // hmr: false,
     strictPort: true,
-    // port: 5173, // Ensure the correct port is set
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
-        // target: "https://ankahe-lafz-backend.onrender.com",
+        target: process.env.VITE_BACKEND_URL || "http://localhost:8000",
         changeOrigin: true,
         secure: false,
       },
     },
   },
 });
-
